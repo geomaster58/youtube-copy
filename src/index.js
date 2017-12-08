42740899dea23b2870d706ed2198110b08c24891
JJ Keeney <jjkeeney129285@gmail.com>
2017-12-08 14:22:28 -0700

    passed data to videolist

--- src/index.js
+++ src/index.js
@@ -1,20 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import SearchBar from "./components/search_bar";
+import VideoList from "./components/video_list";
 import YTSearch from "youtube-api-search";
 
 const API_KEY = "AIzaSyClI-6u5gFnsjAstuOKNhW2174isxWPNEc";
 
-YTSearch({key: API_KEY, term: "surfboards"}, function(data) {
-  console.log(data);
-});
 
-const App = () => {
-  return (
-    <div>
-    <SearchBar />
-  </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {videos: []};
+    YTSearch({key: API_KEY, term: "surfboards"}, (videos) => {
+        this.setState({videos});
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector(".container"));
